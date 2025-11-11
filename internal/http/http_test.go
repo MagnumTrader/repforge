a8300d71ee5460b9f58154b4d4c066fb9e4cfa69
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2025-11-11 22:09:02 +0100

    test sserver pass

--- internal/http/http_test.go
+++ internal/http/http_test.go
@@ -1,15 +1,40 @@
 package http
 
 import (
+	"io"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+func fatalErr(err error, t *testing.T)  {
+	t.Helper()
+	if err != nil {
+	  t.Fatal(err)
+	}
+  
+}
 
 func TestServer(t *testing.T)  {
 
 	// this should be my router
-	httptest.NewServer()
+	r := GetRouter()
+	server := httptest.NewServer(r)
+	defer server.Close()
 
-  
+
+	resp, err := http.Get(server.URL + "/health")
+	fatalErr(err, t)
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("not status 200")
+	}
+
+	body, _ := io.ReadAll(resp.Body)
+
+	if string(body) != "healthy" {
+		t.Fatalf("Expected body 'healthy' got %s", body)
+	}
 }
