d9d928a9cffdf2d3779ca6899040ea90173d0b68
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2025-11-13 22:08:40 +0100

    handling of server restarts aswell

--- internal/http/static/hotreload.js
+++ internal/http/static/hotreload.js
@@ -1,19 +1,43 @@
 let source;
 
 function init() {
-  source = new EventSource("/hotreload");
+    source = new EventSource("/hotreload");
 
-  source.onmessage = (event) => {
-    location.reload()
-  };
+    // A file has been updated
+    source.onmessage = (_) => {
+        location.reload()
+    };
 
-  source.onerror = (error) => {
-    console.log(error);
-  };
+    // This happens when the server errors
+    source.onerror = (_) => {
+        const interval = 200
+        const startTime = Date.now();
+        const maxWait = 8000
+
+        const ping = setInterval(async () => {
+            try {
+                const res = await fetch("/health", { method: "GET" });
+                if (res.ok) {
+                    console.log("Server back! Reloading...");
+                    clearInterval(ping);
+                    location.reload();
+                }
+            } catch {
+                if (Date.now() - startTime > maxWait) {
+                    console.warn(`Server did not respond within 8 seconds`);
+                    clearInterval(ping);
+                    location.reload();
+                }
+            }
+        }, interval);
+    };
 }
 
+// This is for closing the connection to the go server
+// Had some issues with the browser getting conflicted and not 
+// being able to open a new connection
 window.addEventListener("beforeunload", () => {
-  if (source) source.close();
+    if (source) source.close();
 });
 
 init();
