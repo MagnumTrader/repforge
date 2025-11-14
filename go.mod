8d39e4d5c89f56e4583db19139718ba94961afe5
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2025-11-14 23:58:47 +0100

    addedd db as repo

--- go.mod
+++ go.mod
@@ -6,8 +6,9 @@ tool github.com/a-h/templ/cmd/templ
 
 require (
 	github.com/a-h/templ v0.3.960
+	github.com/fsnotify/fsnotify v1.9.0
 	github.com/gin-gonic/gin v1.11.0
-	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.32
 )
 
 require (
@@ -19,7 +20,6 @@ require (
 	github.com/cli/browser v1.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/fatih/color v1.16.0 // indirect
-	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
--- internal/infrastructure/db/db.go
+++ internal/infrastructure/db/db.go
@@ -1,2 +1,78 @@
 package db
 
+import (
+	"database/sql"
+
+	"github.com/MagnumTrader/repforge/internal/domain"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Db struct {
+	inner *sql.DB
+}
+
+func NewDb() *Db {
+	db, err := sql.Open("sqlite3", "data/repforge.db")
+	if err != nil {
+		panic(err)
+	}
+
+	return &Db{
+		inner: db,
+	}
+}
+
+// So this
+
+func (d *Db) GetWorkout(id int) (*domain.Workout, error) {
+	row := d.inner.QueryRow("select id, date, type, duration, notes  from workouts where id=?", id)
+
+	mo := &domain.Workout{
+		Id:       id,
+		Date:     "",
+		Type:     "",
+		Duration: 0,
+		Notes:    "",
+	}
+
+	row.Scan(
+		&mo.Id,
+		&mo.Date,
+		&mo.Type,
+		&mo.Duration,
+		&mo.Notes,
+	)
+	return mo, nil
+}
+func (d *Db) GetAllWorkouts(userId int) ([]domain.Workout, error) {
+	rows, err := d.inner.Query("select id, date, type, duration, notes  from workouts")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	workouts = make([]domain.Workout, 0)
+
+	wo := &domain.Workout{}
+
+	for rows.Next() {
+		err := rows.Scan(
+			&wo.Id,
+			&wo.Date,
+			&wo.Type,
+			&wo.Duration,
+			&wo.Notes)
+
+		if err != nil {
+			return nil, err
+		}
+
+		workouts = append(workouts, *wo)
+
+	}
+
+	return workouts, nil
+}
+func (d *Db) SaveWorkout(workout domain.Workout) error {
+	return nil
+}
