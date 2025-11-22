c90edcd8f308eda3b1db526f9be60ddd485a1006
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2025-11-22 11:37:38 +0100

    remove lang

--- internal/http/ui/pages_templ.go
+++ internal/http/ui/pages_templ.go
@@ -74,7 +74,7 @@ func Base(content ...templ.Component) templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<!doctype html><html lang=\"en-GB\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<!doctype html><html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
